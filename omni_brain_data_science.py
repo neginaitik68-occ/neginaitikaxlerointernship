e0a6fd2be88dd39384f4bf9b5f5f4e40bf671745
neginaitik68-occ <neginaitik68@gmail.com>
2026-07-17 20:46:44 +0530

    Update omni_brain_data_science.py

--- omni_brain_data_science.py
+++ omni_brain_data_science.py
@@ -43,3 +43,28 @@ print("Name: paraphrase-MiniLM-L3-v2")
 print("Shape:", embeddings2.shape)
 print("Time:", end - start)
 print("Memory:", embeddings2.nbytes, "bytes")
+
+#creating third model
+
+model3 = SentenceTransformer("BAAI/bge-small-en-v1.5")
+start = time.time()
+embeddings3 = model3.encode(documents)
+end = time.time()
+print("Model 2")
+print("Name: paraphrase-MiniLM-L3-v2")
+print("Shape:", embeddings3.shape)
+print("Time:", end - start)
+print("Memory:", embeddings3.nbytes, "bytes")
+
+#creating fourth model
+
+model4 = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+start = time.time()
+embeddings4 = model4.encode(documents)
+end = time.time()
+print("Model4")
+print("Name: all-mpnet-base-v2")
+print("Shape:", embeddings4.shape)
+print("Time:", end - start)
+print("Memory:", embeddings4.nbytes, "bytes")
+
